6bc629b17ff4bf0ab165e60e7d4e15152b563730
Bryan Call <bcall@apache.org>
2021-03-15 13:48:55 -0700

    Tool to setup a working remap file

---
+++ new_remap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+grep -v txn_box.so /home/y/conf/trafficserver/remap.config > /tmp/new_remap.config
+lines=$(wc -l /tmp/new_remap.config | awk '{print $1}')
+echo "lines: $lines"
+
+tail_lines=$(expr $lines - 10000)
+echo "tail_lines: $tail_lines"
+
+head -10000 /tmp/new_remap.config > /tmp/new_generator_remap.config
+
+echo "map http://127.0.0.1/cache/ http://127.0.0.1/cache/ @plugin=generator.so" >> /tmp/new_generator_remap.config
+echo "map https://127.0.0.1/cache/ http://127.0.0.1/cache/ @plugin=generator.so" >> /tmp/new_generator_remap.config
+
+tail -$tail_lines /tmp/new_remap.config >> /tmp/new_generator_remap.config
+
+rm /tmp/new_remap.config
+
+sudo mv /tmp/new_generator_remap.config /home/y/conf/trafficserver/remap.config
