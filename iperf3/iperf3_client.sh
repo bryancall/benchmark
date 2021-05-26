d4119d6a3b5641b600b764377186036010603259
Bryan Call <bcall@apache.org>
2021-05-26 10:20:42 -0700

    Fixed argument

--- iperf3/iperf3_client.sh
+++ iperf3/iperf3_client.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-iperf3 -t 30 -c $0 -p 5000 &
-iperf3 -t 30 -c $0 -p 5001 &
-iperf3 -t 30 -c $0 -p 5002 &
-iperf3 -t 30 -c $0 -p 5003 &
-iperf3 -t 30 -c $0 -p 5004 &
-iperf3 -t 30 -c $0 -p 5005 &
-iperf3 -t 30 -c $0 -p 5006 &
-iperf3 -t 30 -c $0 -p 5007 &
-iperf3 -t 30 -c $0 -p 5008 &
-iperf3 -t 30 -c $0 -p 5009 &
+Testing with server $1
+iperf3 -t 30 -c $1 -p 5000 &
+iperf3 -t 30 -c $1 -p 5001 &
+iperf3 -t 30 -c $1 -p 5002 &
+iperf3 -t 30 -c $1 -p 5003 &
+iperf3 -t 30 -c $1 -p 5004 &
+iperf3 -t 30 -c $1 -p 5005 &
+iperf3 -t 30 -c $1 -p 5006 &
+iperf3 -t 30 -c $1 -p 5007 &
+iperf3 -t 30 -c $1 -p 5008 &
+iperf3 -t 30 -c $1 -p 5009 &
