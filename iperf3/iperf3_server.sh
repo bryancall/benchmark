643c15f4ccdd3a1af39c3c3d2ac7940e40b7d4ce
Bryan Call <bcall@apache.org>
2021-05-26 09:54:19 -0700

    iperf3 scripts to run 10 processes

---
+++ iperf3/iperf3_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+iperf3 -s -p 5000 &
+iperf3 -s -p 5001 &
+iperf3 -s -p 5002 &
+iperf3 -s -p 5002 &
+iperf3 -s -p 5003 &
+iperf3 -s -p 5004 &
+iperf3 -s -p 5005 &
+iperf3 -s -p 5006 &
+iperf3 -s -p 5007 &
+iperf3 -s -p 5008 &
+iperf3 -s -p 5009 &
